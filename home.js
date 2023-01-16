26ed11087893257ffc89aae78714cdcd0c4eceea
Carson Hines <97487927+CarsonHines@users.noreply.github.com>
2023-01-15 19:27:29 -0500

    Applied restructured navigation to all pages

--- home.js
+++ home.js
@@ -2,16 +2,16 @@ const navigation = document.getElementById("navigation");
 const desktop = document.getElementById("desktopView");
 const mobile = document.getElementById("mobileView");
 
-function screenSize() {
-    if (window.innerWidth <= 768) {
-        desktop.style.display = "none";
-        mobile.style.display = "inherit";
-    }
-    else {
-        mobile.style.display = "none";
-        desktop.style.display = "inherit";
-    }
-}
+// function screenSize() {
+//     if (window.innerWidth <= 768) {
+//         desktop.style.display = "none";
+//         mobile.style.display = "inherit";
+//     }
+//     else {
+//         mobile.style.display = "none";
+//         desktop.style.display = "inherit";
+//     }
+// }
 
-window.onload = screenSize;
-window.onresize = screenSize;
+// window.onload = screenSize;
+// window.onresize = screenSize;
--- services.js
+++ services.js
@@ -1,68 +1,17 @@
 const navigation = document.getElementById("navigation");
+const desktop = document.getElementById("desktopView");
+const mobile = document.getElementById("mobileView");
 
-function screenSize() {
-    if (window.innerWidth <= 768) {
-        navigation.innerHTML =
-        `<div class="d-flex flex-column flex-shrink-0">
-        <div class="dropdown">
-            <a href="#" class="btn p-2 link-dark text-decoration-none " role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expandede="false" data-bs-placement="right" data-bs-original-title="Icon-only">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#12609b" class="bi bi-hammer" viewBox="0 0 16 16">
-                    <path d="M9.972 2.508a.5.5 0 0 0-.16-.556l-.178-.129a5.009 5.009 0 0 0-2.076-.783C6.215.862 4.504 1.229 2.84 3.133H1.786a.5.5 0 0 0-.354.147L.146 4.567a.5.5 0 0 0 0 .706l2.571 2.579a.5.5 0 0 0 .708 0l1.286-1.29a.5.5 0 0 0 .146-.353V5.57l8.387 8.873A.5.5 0 0 0 14 14.5l1.5-1.5a.5.5 0 0 0 .017-.689l-9.129-8.63c.747-.456 1.772-.839 3.112-.839a.5.5 0 0 0 .472-.334z"/>
-                </svg>
-                <span class="visually-hidden">Icon-only</span>
-            </a>
-            <ul class="dropdown-menu flex-column mb-auto align-content-center py-0 my-2" style="width: 4.6rem" aria-labelledby="dropdownMenuLink">
-                <li>
-                    <a href="/home.html" id="dropdownLogos" class="dropdown-item border-bottom rounded-0 justify-content-center py-2 px-2" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Home" data-bs-original-title="Home">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="bi bi-house-door" fill="currentColor" viewBox="0 0 16 16" role="img">
-                            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
-                        </svg>
-                    </a>
-                </li>
-                <li>
-                    <a href="/services.html" id="dropdownLogos" class="dropdown-item py-1 border-bottom rounded-0 justify-content-center py-2 px-2" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Services" data-bs-original-title="Services">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-tools" width="24" height="24" viewBox="0 0 16 16" fill="#009848" role="img">
-                            <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/>
-                        </svg>
-                    </a>
-                </li>
-                <li>
-                    <a href="/contact.html" id="dropdownLogos" class="dropdown-item py-1 border-bottom rounded-0 justify-content-center py-2 px-2" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Contact" data-bs-original-title="Contact">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="bi bi-envelope" viewBox="0 0 16 16" fill="currentColor" role="img">
-                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-                        </svg>
-                    </a>
-                </li>
-            </ul>
-            </div>
-        </div>`;
-    }
+// function screenSize() {
+//     if (window.innerWidth <= 768) {
+//         desktop.style.display = "none";
+//         mobile.style.display = "inherit";
+//     }
+//     else {
+//         mobile.style.display = "none";
+//         desktop.style.display = "inherit";
+//     }
+// }
 
-    else {
-        navigation.innerHTML =
-        `<header class="d-flex flex-wrap justify-content-between py-3" id="logoTitle">
-            <a href="/home.html" class="d-flex align-items-center mb-md-0 me-md-auto text-decoration-none">
-                <!--Hammer Icon-->
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" fill="#12609b" class="bi bi-hammer" viewBox="0 0 16 16">
-                    <path d="M9.972 2.508a.5.5 0 0 0-.16-.556l-.178-.129a5.009 5.009 0 0 0-2.076-.783C6.215.862 4.504 1.229 2.84 3.133H1.786a.5.5 0 0 0-.354.147L.146 4.567a.5.5 0 0 0 0 .706l2.571 2.579a.5.5 0 0 0 .708 0l1.286-1.29a.5.5 0 0 0 .146-.353V5.57l8.387 8.873A.5.5 0 0 0 14 14.5l1.5-1.5a.5.5 0 0 0 .017-.689l-9.129-8.63c.747-.456 1.772-.839 3.112-.839a.5.5 0 0 0 .472-.334z"/>
-                </svg>
-                <!--Company Name-->
-                <span class="fs-4">Honey Do Helpers</span>
-            </a>
-            <ul class="nav nav-masthead" id="desktop">
-                <li class="nav-item">
-                    <a href="/home.html" class="nav-link text-light" aria-current="home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/services.html" class="nav-link active mx-3">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/contact.html" class="nav-link text-light">Contact</a>
-                </li>
-            </ul>
-        </header>`;
-    }
-}
-
-window.onload = screenSize;
-window.onresize = screenSize;
+// window.onload = screenSize;
+// window.onresize = screenSize;
