551cb50a7f8d51036e7e7cd0fdcb29f36a1fbb1a
Carson hines <harsoncines@gmail.com>
2023-03-15 20:22:43 -0400

    Started Contact page and php

--- home.js
+++
@@ -1,7 +0,0 @@
-function contactPage() {
-    window.location.href = "https://honeydohelpers.net/contact/";
-}
-
-const contactButton = document.getElementById("bannerContact");
-
-contactButton.addEventListener("click", contactPage)
